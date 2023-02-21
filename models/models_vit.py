5334df5edb26b677bad2e1f437c5ad05369e27c2
arexh <644564286@qq.com>
2023-02-21 18:33:30 +0800

    fix model_vit.py

--- models/models_vit.py
+++ models/models_vit.py
@@ -23,16 +23,15 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """
     def __init__(self, pooling='mean', mask_ratio=.0, **kwargs):
         super(VisionTransformer, self).__init__(**kwargs)
-        
-        # masking initialization
         self.mask_ratio = mask_ratio
-
         # pooling preperation
         self.pooling = pooling
         if self.pooling != 'cls':
             norm_layer = kwargs['norm_layer']
             embed_dim = kwargs['embed_dim']
             del self.norm  # remove the original norm
+
+        # pooling
         if self.pooling == 'mean':
             self.fc_norm = norm_layer(embed_dim)
         elif self.pooling == 'nextvlad':
@@ -57,23 +56,26 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
         
     def random_masking(self, x, mask_ratio):
         B, L, D = x.size()
-        len_keep = int(L * (1 - mask_ratio)) if self.training else L
+        len_keep = int(L * (1 - mask_ratio))
 
-        # get random sequence
         noise = torch.rand(B, L, device=x.device)  # noise in [0, 1]
+
+        # sort noise for each sample
         ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-        ids_restore = torch.argsort(ids_shuffle, dim=1)
+        ids_restore = torch.argsort(ids_shuffle, dim=1) # [B, L]
+
+        # keep the first subset
         ids_keep = ids_shuffle[:, :len_keep]
-        
         # mask input
-        x = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
 
         # generate the binary mask: 0 is remove, 1 is keep, opposite with pretrain
         mask = torch.zeros([B, L], device=x.device)
         mask[:, :len_keep] = 1
+        # unshuffle to get the binary mask
         mask = torch.gather(mask, dim=1, index=ids_restore)
 
-        return x, mask, ids_restore
+        return x_masked, mask, ids_restore
     
     def forward_features(self, x):
         # embed patches
@@ -82,16 +84,18 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
 
         # add pos embed w/o cls token
         x = x + self.pos_embed[:, 1:, :]
-        x = self.pos_drop(x)
-
+        
         # masking
         mask_ratio = self.mask_ratio if self.training else 0
         x, mask, ids_restore = self.random_masking(x, mask_ratio)
 
-        # cls token
+        # add cls token
         cls_token = self.cls_token + self.pos_embed[:, :1, :]
         cls_tokens = cls_token.expand(B, -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
+
+        # dropout
+        x = self.pos_drop(x)
 
         # forward
         for blk in self.blocks:
@@ -99,18 +103,16 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
 
         # pooling
         if self.pooling == 'mean':
-            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
+            x = x[:, 1:, :].mean(dim=1)  # global pooling without cls token
             outcome = self.fc_norm(x)
         elif self.pooling == 'cls':
             x = self.norm(x)
             outcome = x[:, 0]
         elif self.pooling == 'nextvlad':
-            # remove cls token
-            x = x[:, 1:, :]
             # append zero tokens to sequence
-            zero_tokens = torch.zeros([B, ids_restore.shape[1] - x.shape[1], D], device=x.device)
-            x = torch.cat([x, zero_tokens], dim=1)  # no cls token
-            x = torch.gather(x, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+            zero_tokens = torch.zeros([B, ids_restore.shape[1] + 1 - x.shape[1], D], device=x.device, dtype=x.dtype)
+            x_ = torch.cat([x[:, 1:, :], zero_tokens], dim=1)  # no cls token
+            x = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, D))  # unshuffle [B, L, D]
             # nextvlad
             x = self.before_norm(x)
             x = self.nextvlad(x, mask)
@@ -122,6 +124,12 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
             assert False, 'Wrong pooling value!'
 
         return outcome
+    
+    def forward(self, x):
+        rep = self.forward_features(x)
+        ce = self.head(rep)
+        return ce
+
 
 
 def vit_base_patch16(**kwargs):
